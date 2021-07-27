27351349b39d3ba5fc1b16b0ad3a11bc27da0434
vlepekha <vlepekha@student.ucode.world>
2021-07-27 15:22:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.f68af143be46621dedf87521c6310985.js"
 );
 
 self.addEventListener('message', (event) => {
